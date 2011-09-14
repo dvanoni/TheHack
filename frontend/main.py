aa720a39606c6bc57041835b9504a5514b2e112f
Matthew Peterson <mateo42@gmail.com>
2011-09-13 22:32:29 -0400

    sessions!!!!!

--- frontend/main.py
+++ frontend/main.py
@@ -1,9 +1,16 @@
 import bottle
-from bottle import template, static_file
+from bottle import template, static_file, request
 from settings import STATIC_PATH
 
 FRONT_END = bottle.Bottle()
 
 @FRONT_END.route( '/index' )
 def front_end_index():
-    return template( 'index' )
+	login = True
+	s = bottle.request.environ.get('beaker.session')
+	username = s.get('username')
+
+	if username:
+		login = False
+
+	return template('index', login=login, username=username)
